62aa17343417a56a6c2385b4d5f3d42585ad314e
arita37 <noelkev0@gmail.com>
2022-09-10 12:31:28 +0900

    ok

--- docs_ghpage/conf.py
+++ docs_ghpage/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'utilmy'
+project = 'utilmy2'
 copyright = ''
 author = ''
 
@@ -143,7 +143,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'utilmydoc'
+htmlhelp_basename = 'utilmy2doc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -170,7 +170,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'utilmy.tex', 'utilmy Documentation',
+    (master_doc, 'utilmy2.tex', 'utilmy2 Documentation',
      'Michael Altfield', 'manual'),
 ]
 
@@ -180,7 +180,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'utilmy', 'utilmy Documentation',
+    (master_doc, 'utilmy2', 'utilmy2 Documentation',
      [author], 1)
 ]
 
@@ -191,7 +191,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'utilmy', 'utilmy Documentation',
+    (master_doc, 'utilmy2', 'utilmy2 Documentation',
      author, 'utilmy', 'One line description of project.',
      'Miscellaneous'),
 ]
